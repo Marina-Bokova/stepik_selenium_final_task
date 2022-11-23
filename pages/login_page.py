06b1f44106e522b2923a0d0a5488aee95422fb7a
Marina <mary.v.bokova@gmail.com>
2022-11-24 00:10:43 +0700

    Fix bug in selectors and method names

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,14 +3,14 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
-    def login_page_is_closed(self):
+    def login_page_is_valid(self):
         self.url_contains_login_str()
         self.login_form_is_presented()
         self.register_form_is_presented()
 
     def url_contains_login_str(self):
         url_page = self.browser.current_url
-        assert "login1" in url_page, \
+        assert "login" in url_page, \
             "Failed to go to login page"
 
     def login_form_is_presented(self):
